48df8c6408d5faa992ca3e41f6e5b672e42a4cd0
龙骑士尹道长 <523580265@qq.com>
2023-06-05 11:29:53 +0800

    ffdddddddddsddddddfffss

--- src/pages/Editor/material/system/AlarmCardGroup/defaultValue.ts
+++ src/pages/Editor/material/system/AlarmCardGroup/defaultValue.ts
@@ -1,23 +1,37 @@
-import type {ElementType} from '@/pages/Editor/Types';
-const defaultValue:ElementType = {
+import type { ElementType } from '@/pages/Editor/Types';
+
+const defaultValue: ElementType = {
   label: '警报卡片组',
   type: 'container',
-  value:'AlarmCardGroup',
-  id:'',
-  icon:'icon-qiapianshuliang',
+  value: 'AlarmCardGroup',
+  id: '',
+  icon: 'icon-qiapianshuliang',
   data: {
-    datasource:{"result":0,"message":"成功！","guid":null,"data":{"waringCount":{"value":"322","name":"报警总数"},"eliminatedCount":{"value":"318","name":"已消除报警"},"conversionRate":{"value":"98.7","name":"报警转化率"},"unconfirmedCount":{"value":"261","name":"未确认报警"},"confirmedCount":{"value":"61","name":"已确认报警"},"maxFloor":{"value":"B1F","name":"报警高发楼层"},"maxDevice":{"value":"空调机组","name":"报警高发设备类型"}}},
-    attribute:{
-      size:'default',
-      style:'left',
-      space:'default',
-      iconSize:'default',
-      badge:true,
+    datasource: {
+      'result': 0,
+      'message': '成功！',
+      'guid': null,
+      'data': {
+        'waringCount': { 'value': '322', 'name': '报警总数' },
+        'eliminatedCount': { 'value': '318', 'name': '已消除报警' },
+        'conversionRate': { 'value': '98.7', 'name': '报警转化率' },
+        'unconfirmedCount': { 'value': '261', 'name': '未确认报警' },
+        'confirmedCount': { 'value': '61', 'name': '已确认报警' },
+        'maxFloor': { 'value': 'B1F', 'name': '报警高发楼层' },
+        'maxDevice': { 'value': '空调机组', 'name': '报警高发设备类型' },
+      },
     },
-    onlineXHR:{
-      list:{isOnline:false,url:'/api/material/alarmCardGroup/list'},
-    }
+    attribute: {
+      size: 'default',
+      style: 'left',
+      space: 'default',
+      iconSize: 'default',
+      badge: true,
+    },
+    onlineXHR: {
+      list: { isOnline: false, url: '/api/material/alarmCardGroup/list' },
+    },
   },
-}
+};
 
-export default defaultValue
+export default defaultValue;
